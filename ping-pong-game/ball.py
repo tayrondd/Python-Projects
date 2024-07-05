86c564092b7f8cd95bf0fb3d85e7b7292e269100
tayrondd <77041022+tayrondd@users.noreply.github.com>
2024-07-05 17:31:58 -0400

    Ping Pong game

---
+++ ping-pong-game/ball.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("white")
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        x_position = self.xcor() + self.x_move
+        y_position = self.ycor() + self.y_move
+        self.goto(x_position, y_position)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.y_move *= -1
+        self.x_move *= -1
---
+++ ping-pong-game/main.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-350, 0)
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
+
+play = True
+while play:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect ball collision between horizontal wall and pong
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
